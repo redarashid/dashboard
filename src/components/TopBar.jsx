19c27a91a985457783a8d77ce9f02b83acf09636
redarashid <redarashid824@gmail.com>
2024-10-20 17:24:27 +0300

    first commit

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,11 @@
-import { IconButton, styled, Toolbar, Typography } from '@mui/material'
+import {IconButton, InputBase, styled, Toolbar } from '@mui/material'
 import React from 'react'
 import MenuIcon from "@mui/icons-material/Menu";
 import MuiAppBar from '@mui/material/AppBar';
+import {alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 
 const drawerWidth = 240;
 
@@ -30,6 +34,46 @@ const AppBar = styled(MuiAppBar, {
   ],
 }));
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {  
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
 const TopBar = ({open, handleDrawerOpen}) => {
   return (
     <AppBar position="fixed" 
@@ -50,9 +94,17 @@ const TopBar = ({open, handleDrawerOpen}) => {
               >
                 <MenuIcon />
               </IconButton>
-              <Typography variant="h6" noWrap component="div">
-                Mini variant drawer
-              </Typography>
+
+
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
             </Toolbar>
         </AppBar> 
   )
