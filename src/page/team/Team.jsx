65835ead874006da738cc059fe195acf44725589
redarashid <redarashid824@gmail.com>
2024-10-22 01:58:56 +0300

    first commit

--- src/page/team/Team.jsx
+++ src/page/team/Team.jsx
@@ -1,23 +1,46 @@
 import { DataGrid} from '@mui/x-data-grid';
 import React from 'react'
+import { rows } from './Data';
+import { Box, Typography, useTheme } from '@mui/material';
 
 
 
-const rows = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
-
-const columns = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
-];
 
 const Team = () => {
+  const theme = useTheme()
+
+
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 33, align: "center", headerAlign: "center" },
+    { field: 'name', headerName: 'Name', flex: 1, align: "center", headerAlign: "center" },
+    { field: 'email', headerName: 'Email', flex: 1, align: "center", headerAlign: "center" },
+    { field: 'age', headerName: 'Age', flex: 1, align: "center", headerAlign: "center" },
+    { field: 'phone', headerName: 'Phone', flex: 1, align: "center", headerAlign: "center" },
+    { field: 'access', headerName: 'Access', flex: 1, align: "center", headerAlign: "center",
+      renderCell: ({row: {access}}) => {
+        return (
+          <Box sx={{
+            p: "5px",
+            width: "99px",
+            borderRadius: "5px",
+            textAlign: "center",
+            backgroundColor: theme.palette.primary.dark
+          }}>
+            <Typography>{access}</Typography>
+          </Box>
+        )
+      }
+    },
+  ];
+
+
+  
   return (
-    <div style={{ height: 300, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} />
+    <div style={{ height: 650, width: '100%' }}>
+      <DataGrid rows={rows} 
+// @ts-ignore
+      columns={columns} />
     </div>
   )
 }
