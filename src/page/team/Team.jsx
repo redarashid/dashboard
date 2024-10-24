54d03db2705dd27423c4443b8ef336e0ad5c6ea8
redarashid <redarashid824@gmail.com>
2024-10-24 14:32:07 +0300

    first commit

--- src/page/team/Team.jsx
+++ src/page/team/Team.jsx
@@ -1,48 +1,110 @@
-import { DataGrid} from '@mui/x-data-grid';
-import React from 'react'
-import { rows } from './Data';
-import { Box, Typography, useTheme } from '@mui/material';
 
 
-
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { rows } from "./Data";
+import { useTheme } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import {
+  AdminPanelSettingsOutlined,
+  LockOpenOutlined,
+  SecurityOutlined,
+} from "@mui/icons-material";
 
 const Team = () => {
-  const theme = useTheme()
+  const theme = useTheme();
 
-
-
+  // field ==> Reqird
   const columns = [
-    { field: 'id', headerName: 'ID', width: 33, align: "center", headerAlign: "center" },
-    { field: 'name', headerName: 'Name', flex: 1, align: "center", headerAlign: "center" },
-    { field: 'email', headerName: 'Email', flex: 1, align: "center", headerAlign: "center" },
-    { field: 'age', headerName: 'Age', flex: 1, align: "center", headerAlign: "center" },
-    { field: 'phone', headerName: 'Phone', flex: 1, align: "center", headerAlign: "center" },
-    { field: 'access', headerName: 'Access', flex: 1, align: "center", headerAlign: "center",
-      renderCell: ({row: {access}}) => {
+    {
+      field: "id",
+      headerName: "ID",
+      width: 33,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "name",
+      headerName: "name",
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "email",
+      headerName: "email",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    { field: "age", headerName: "age", align: "center", headerAlign: "center" },
+    {
+      field: "phone",
+      headerName: "phone",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "access",
+      headerName: "access",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: ({ row: { access } }) => {
         return (
-          <Box sx={{
-            p: "5px",
-            width: "99px",
-            borderRadius: "5px",
-            textAlign: "center",
-            backgroundColor: theme.palette.primary.dark
-          }}>
-            <Typography>{access}</Typography>
+          <Box
+            sx={{
+              p: "5px",
+              width: "99px",
+              borderRadius: "3px",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "space-evenly",
+
+              backgroundColor:
+                access === "Admin"
+                  ? theme.palette.primary.dark
+                  : access === "Manager"
+                  ? theme.palette.secondary.dark
+                  : "#3da58a",
+            }}
+          >
+            {access === "Admin" && (
+              <AdminPanelSettingsOutlined
+                sx={{ color: "#fff" }}
+                fontSize="small"
+              />
+            )}
+
+            {access === "Manager" && (
+              <SecurityOutlined sx={{ color: "#fff" }} fontSize="small" />
+            )}
+
+            {access === "User" && (
+              <LockOpenOutlined sx={{ color: "#fff" }} fontSize="small" />
+            )}
+
+            <Typography sx={{ fontSize: "13px", color: "#fff" }}>
+              {access}
+            </Typography>
           </Box>
-        )
-      }
+        );
+      },
     },
   ];
 
-
-  
   return (
-    <div style={{ height: 650, width: '100%' }}>
-      <DataGrid rows={rows} 
-// @ts-ignore
-      columns={columns} />
-    </div>
-  )
-}
+    <Box>
 
-export default Team
+      <Box sx={{ height: 600, mx: "auto" }}>
+        <DataGrid
+          rows={rows}
+          // @ts-ignore
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Team;
