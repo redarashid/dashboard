6369281877920b791f6a88ec06508259fa6d9f94
redarashid <redarashid824@gmail.com>
2024-10-24 15:17:10 +0300

    first commit

--- src/page/invoices/Invoices.jsx
+++ src/page/invoices/Invoices.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import { columns, rows } from "./Data";
 
 const Invoices = () => {
   return (
-    <div>
-        Invoices Page
-    </div>
+    <Box sx={{ height: 600, width: "98%" , mx: "auto" }}>
+    <DataGrid
+        checkboxSelection
+        slots={{
+          toolbar: GridToolbar,
+        }}
+      rows={rows}
+      // @ts-ignore
+      columns={columns}
+    />
+  </Box>
   )
 }
 
