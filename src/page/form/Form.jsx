17c6ae4ff8c390f3151a2b45ae00eb4f97cdd340
redarashid <redarashid824@gmail.com>
2024-10-24 18:21:04 +0300

    first commit

--- src/page/form/Form.jsx
+++ src/page/form/Form.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { Stack } from '@mui/material';
-import MenuItem from '@mui/material/MenuItem';
-
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Alert, Button, Snackbar, Stack } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import { useForm } from "react-hook-form";
 
 const data = [
   {
-    value: 'Admin',
-    label: 'Admin',
+    value: "Admin",
+    label: "Admin",
   },
   {
-    value: 'Manager',
-    label: 'Manager',
+    value: "Manager",
+    label: "Manager",
   },
   {
-    value: 'User',
-    label: 'User',
-  }
+    value: "User",
+    label: "User",
+  },
 ];
 
+const regEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const onSubmit = () => {
+    console.log("doneee");
+
+    handleClick();
+  };
+
   return (
     <Box
+      onSubmit={handleSubmit(onSubmit)}
       component="form"
       // sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
       sx={{
@@ -31,33 +64,85 @@ const Form = () => {
         gap: 3,
       }}
       noValidate
-      autoComplete="off"
-    >
-    <Stack direction={"row"}>
-      <TextField  label="First Name" variant="filled" sx={{flex: 1, mr: "15px"}}  />
-      <TextField  label="Last Name" variant="filled" sx={{flex: 1, mr: "15px"}} />
-    </Stack>
-
-      <TextField  label="Email" variant="filled" />
-      <TextField  label="Contact Number" variant="filled" />
-      <TextField  label="Adress 1" variant="filled" />
-      <TextField  label="Adress 2" variant="filled" />
+      autoComplete="off">
+      <Stack direction={"row"}>
+        <TextField
+          error={Boolean(errors.firstName)}
+          helperText={
+            Boolean(errors.firstName)
+              ? "This field is required & min 3 character"
+              : null
+          }
+          {...register("firstName", { required: true, minLength: 3 })}
+          label="First Name"
+          variant="filled"
+          sx={{ flex: 1, mr: "15px" }}
+        />
+        <TextField
+          error={Boolean(errors.lastName)}
+          helperText={
+            Boolean(errors.lastName)
+              ? "This field is required & min 3 character"
+              : null
+          }
+          {...register("lastName", { required: true, minLength: 3 })}
+          label="Last Name"
+          variant="filled"
+          sx={{ flex: 1 }}
+        />
+      </Stack>
 
       <TextField
-          variant="filled"
-          select
-          label="Role"
-          defaultValue="User"
-        >
-          {data.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        error={Boolean(errors.email)}
+        helperText={
+          Boolean(errors.email) ? "Please provide a valid email address" : null
+        }
+        {...register("email", { required: true, pattern: regEmail })}
+        label="Email"
+        variant="filled"
+      />
+      <TextField
+        error={Boolean(errors.contactNumber)}
+        helperText={
+          Boolean(errors.contactNumber)
+            ? "Please provide a valid Phone number"
+            : null
+        }
+        {...register("contactNumber", { required: true, pattern: phoneRegExp })}
+        label="Contact Number"
+        variant="filled"
+      />
+      <TextField label="Adress 1" variant="filled" />
+      <TextField label="Adress 2" variant="filled" />
+
+      <TextField variant="filled" select label="Role" defaultValue="User">
+        {data.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <Box sx={{ textAlign: "right" }}>
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ textTransform: "capitalize" }}>
+          create new user
+        </Button>
+
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}>
+          <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
+            Account created successfully
+          </Alert>
+        </Snackbar>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Form
-
+export default Form;
