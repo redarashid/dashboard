552e425d8578547cdcc602894aff85cec86a38c2
redarashid <redarashid824@gmail.com>
2024-10-26 00:52:21 +0300

    first commit

--- src/page/geography/Geography.jsx
+++ src/page/geography/Geography.jsx
@@ -4,14 +4,16 @@ import { ResponsiveChoropleth } from "@nivo/geo";
 import { geo } from "./world_countries";
 import { data } from "./Data";
 
-
-
 const Geography = () => {
-
   const theme = useTheme();
 
   return (
-    <Box sx={{ height: "75vh" }}>
+    <Box
+      sx={{
+        height: "75vh",
+        border: `1px solid ${theme.palette.text.primary}`,
+        borderRadius: "3px",
+      }}>
       <ResponsiveChoropleth
         data={data}
         theme={{
@@ -124,72 +126,19 @@ const Geography = () => {
           },
         }}
         features={geo.features}
+        projectionScale={130}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        colors="nivo"
+        colors="RdBu"
         domain={[0, 1000000]}
         unknownColor="#666666"
         label="properties.name"
         valueFormat=".2s"
         projectionTranslation={[0.5, 0.5]}
         projectionRotation={[0, 0, 0]}
-        enableGraticule={true}
+        enableGraticule={false}
         graticuleLineColor="#dddddd"
         borderWidth={0.5}
         borderColor="#152538"
-        defs={[
-          {
-            id: "dots",
-            type: "patternDots",
-            background: "inherit",
-            color: "#38bcb2",
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: "lines",
-            type: "patternLines",
-            background: "inherit",
-            color: "#eed312",
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-          {
-            id: "gradient",
-            type: "linearGradient",
-            colors: [
-              {
-                offset: 0,
-                color: "#000",
-              },
-              {
-                offset: 100,
-                color: "inherit",
-              },
-            ],
-          },
-        ]}
-        fill={[
-          {
-            match: {
-              id: "CAN",
-            },
-            id: "dots",
-          },
-          {
-            match: {
-              id: "CHN",
-            },
-            id: "lines",
-          },
-          {
-            match: {
-              id: "ATA",
-            },
-            id: "gradient",
-          },
-        ]}
         legends={[
           {
             anchor: "bottom-left",
@@ -201,7 +150,7 @@ const Geography = () => {
             itemWidth: 94,
             itemHeight: 18,
             itemDirection: "left-to-right",
-            itemTextColor: "#444444",
+            itemTextColor: theme.palette.text.primary,
             itemOpacity: 0.85,
             symbolSize: 18,
             effects: [
