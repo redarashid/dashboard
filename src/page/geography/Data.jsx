d17579b8ebdcfa4d42267936d8f85cfef0552fee
redarashid <redarashid824@gmail.com>
2024-10-26 00:32:18 +0300

    first commit

---
+++ src/page/geography/Data.jsx
@@ -0,0 +1,686 @@
+export const data =  [
+    {
+      id: "AFG",
+      value: 54784,
+    },
+    {
+      id: "AGO",
+      value: 111988,
+    },
+    {
+      id: "ALB",
+      value: 822298,
+    },
+    {
+      id: "ARE",
+      value: 757358,
+    },
+    {
+      id: "ARG",
+      value: 578561,
+    },
+    {
+      id: "ARM",
+      value: 202731,
+    },
+    {
+      id: "ATA",
+      value: 315667,
+    },
+    {
+      id: "ATF",
+      value: 78133,
+    },
+    {
+      id: "AUT",
+      value: 777772,
+    },
+    {
+      id: "AZE",
+      value: 334271,
+    },
+    {
+      id: "BDI",
+      value: 265523,
+    },
+    {
+      id: "BEL",
+      value: 878855,
+    },
+    {
+      id: "BEN",
+      value: 928458,
+    },
+    {
+      id: "BFA",
+      value: 298559,
+    },
+    {
+      id: "BGD",
+      value: 407068,
+    },
+    {
+      id: "BGR",
+      value: 674003,
+    },
+    {
+      id: "BHS",
+      value: 311715,
+    },
+    {
+      id: "BIH",
+      value: 674956,
+    },
+    {
+      id: "BLR",
+      value: 961708,
+    },
+    {
+      id: "BLZ",
+      value: 721277,
+    },
+    {
+      id: "BOL",
+      value: 524889,
+    },
+    {
+      id: "BRN",
+      value: 45964,
+    },
+    {
+      id: "BTN",
+      value: 111391,
+    },
+    {
+      id: "BWA",
+      value: 631054,
+    },
+    {
+      id: "CAF",
+      value: 527886,
+    },
+    {
+      id: "CAN",
+      value: 182664,
+    },
+    {
+      id: "CHE",
+      value: 969978,
+    },
+    {
+      id: "CHL",
+      value: 584254,
+    },
+    {
+      id: "CHN",
+      value: 105785,
+    },
+    {
+      id: "CIV",
+      value: 310768,
+    },
+    {
+      id: "CMR",
+      value: 25716,
+    },
+    {
+      id: "COG",
+      value: 811697,
+    },
+    {
+      id: "COL",
+      value: 959302,
+    },
+    {
+      id: "CRI",
+      value: 406788,
+    },
+    {
+      id: "CUB",
+      value: 31637,
+    },
+    {
+      id: "-99",
+      value: 942036,
+    },
+    {
+      id: "CYP",
+      value: 374688,
+    },
+    {
+      id: "CZE",
+      value: 359209,
+    },
+    {
+      id: "DEU",
+      value: 505984,
+    },
+    {
+      id: "DJI",
+      value: 399854,
+    },
+    {
+      id: "DNK",
+      value: 747491,
+    },
+    {
+      id: "DOM",
+      value: 595712,
+    },
+    {
+      id: "DZA",
+      value: 560505,
+    },
+    {
+      id: "ECU",
+      value: 799128,
+    },
+    {
+      id: "EGY",
+      value: 844255,
+    },
+    {
+      id: "ERI",
+      value: 278316,
+    },
+    {
+      id: "ESP",
+      value: 540199,
+    },
+    {
+      id: "EST",
+      value: 784219,
+    },
+    {
+      id: "ETH",
+      value: 832864,
+    },
+    {
+      id: "FIN",
+      value: 752882,
+    },
+    {
+      id: "FJI",
+      value: 515606,
+    },
+    {
+      id: "FLK",
+      value: 732586,
+    },
+    {
+      id: "FRA",
+      value: 389172,
+    },
+    {
+      id: "GAB",
+      value: 380920,
+    },
+    {
+      id: "GBR",
+      value: 312751,
+    },
+    {
+      id: "GEO",
+      value: 413692,
+    },
+    {
+      id: "GHA",
+      value: 885341,
+    },
+    {
+      id: "GIN",
+      value: 232095,
+    },
+    {
+      id: "GMB",
+      value: 246127,
+    },
+    {
+      id: "GNB",
+      value: 871286,
+    },
+    {
+      id: "GNQ",
+      value: 791691,
+    },
+    {
+      id: "GRC",
+      value: 521923,
+    },
+    {
+      id: "GTM",
+      value: 895452,
+    },
+    {
+      id: "GUY",
+      value: 534541,
+    },
+    {
+      id: "HND",
+      value: 337167,
+    },
+    {
+      id: "HRV",
+      value: 378849,
+    },
+    {
+      id: "HTI",
+      value: 43825,
+    },
+    {
+      id: "HUN",
+      value: 898128,
+    },
+    {
+      id: "IDN",
+      value: 478995,
+    },
+    {
+      id: "IND",
+      value: 821561,
+    },
+    {
+      id: "IRL",
+      value: 861628,
+    },
+    {
+      id: "IRN",
+      value: 923459,
+    },
+    {
+      id: "IRQ",
+      value: 714536,
+    },
+    {
+      id: "ISL",
+      value: 126518,
+    },
+    {
+      id: "ISR",
+      value: 35774,
+    },
+    {
+      id: "ITA",
+      value: 786893,
+    },
+    {
+      id: "JAM",
+      value: 809549,
+    },
+    {
+      id: "JOR",
+      value: 813513,
+    },
+    {
+      id: "JPN",
+      value: 510869,
+    },
+    {
+      id: "KAZ",
+      value: 447433,
+    },
+    {
+      id: "KEN",
+      value: 353219,
+    },
+    {
+      id: "KGZ",
+      value: 43908,
+    },
+    {
+      id: "KHM",
+      value: 424176,
+    },
+    {
+      id: "OSA",
+      value: 968582,
+    },
+    {
+      id: "KWT",
+      value: 792909,
+    },
+    {
+      id: "LAO",
+      value: 288467,
+    },
+    {
+      id: "LBN",
+      value: 44311,
+    },
+    {
+      id: "LBR",
+      value: 683545,
+    },
+    {
+      id: "LBY",
+      value: 986188,
+    },
+    {
+      id: "LKA",
+      value: 416827,
+    },
+    {
+      id: "LSO",
+      value: 907430,
+    },
+    {
+      id: "LTU",
+      value: 464634,
+    },
+    {
+      id: "LUX",
+      value: 978159,
+    },
+    {
+      id: "LVA",
+      value: 726801,
+    },
+    {
+      id: "MAR",
+      value: 858897,
+    },
+    {
+      id: "MDA",
+      value: 808606,
+    },
+    {
+      id: "MDG",
+      value: 957858,
+    },
+    {
+      id: "MEX",
+      value: 144553,
+    },
+    {
+      id: "MKD",
+      value: 316551,
+    },
+    {
+      id: "MLI",
+      value: 308712,
+    },
+    {
+      id: "MMR",
+      value: 554160,
+    },
+    {
+      id: "MNE",
+      value: 146249,
+    },
+    {
+      id: "MNG",
+      value: 291425,
+    },
+    {
+      id: "MOZ",
+      value: 504558,
+    },
+    {
+      id: "MRT",
+      value: 823827,
+    },
+    {
+      id: "MWI",
+      value: 254842,
+    },
+    {
+      id: "MYS",
+      value: 332463,
+    },
+    {
+      id: "NAM",
+      value: 566565,
+    },
+    {
+      id: "NCL",
+      value: 590914,
+    },
+    {
+      id: "NER",
+      value: 47066,
+    },
+    {
+      id: "NGA",
+      value: 723659,
+    },
+    {
+      id: "NIC",
+      value: 153834,
+    },
+    {
+      id: "NLD",
+      value: 463846,
+    },
+    {
+      id: "NOR",
+      value: 220667,
+    },
+    {
+      id: "NPL",
+      value: 486958,
+    },
+    {
+      id: "NZL",
+      value: 396377,
+    },
+    {
+      id: "OMN",
+      value: 200389,
+    },
+    {
+      id: "PAK",
+      value: 633874,
+    },
+    {
+      id: "PAN",
+      value: 674094,
+    },
+    {
+      id: "PER",
+      value: 567120,
+    },
+    {
+      id: "PHL",
+      value: 972423,
+    },
+    {
+      id: "PNG",
+      value: 255301,
+    },
+    {
+      id: "POL",
+      value: 824727,
+    },
+    {
+      id: "PRI",
+      value: 853111,
+    },
+    {
+      id: "PRT",
+      value: 97674,
+    },
+    {
+      id: "PRY",
+      value: 778311,
+    },
+    {
+      id: "QAT",
+      value: 998074,
+    },
+    {
+      id: "ROU",
+      value: 140401,
+    },
+    {
+      id: "RUS",
+      value: 359853,
+    },
+    {
+      id: "RWA",
+      value: 514201,
+    },
+    {
+      id: "ESH",
+      value: 602842,
+    },
+    {
+      id: "SAU",
+      value: 100417,
+    },
+    {
+      id: "SDN",
+      value: 870217,
+    },
+    {
+      id: "SDS",
+      value: 49392,
+    },
+    {
+      id: "SEN",
+      value: 682888,
+    },
+    {
+      id: "SLB",
+      value: 682498,
+    },
+    {
+      id: "SLE",
+      value: 925624,
+    },
+    {
+      id: "SLV",
+      value: 222144,
+    },
+    {
+      id: "ABV",
+      value: 220189,
+    },
+    {
+      id: "SOM",
+      value: 841230,
+    },
+    {
+      id: "SRB",
+      value: 608307,
+    },
+    {
+      id: "SUR",
+      value: 201195,
+    },
+    {
+      id: "SVK",
+      value: 868876,
+    },
+    {
+      id: "SVN",
+      value: 548111,
+    },
+    {
+      id: "SWZ",
+      value: 789177,
+    },
+    {
+      id: "SYR",
+      value: 260556,
+    },
+    {
+      id: "TCD",
+      value: 576199,
+    },
+    {
+      id: "TGO",
+      value: 305017,
+    },
+    {
+      id: "THA",
+      value: 677056,
+    },
+    {
+      id: "TJK",
+      value: 331570,
+    },
+    {
+      id: "TKM",
+      value: 339436,
+    },
+    {
+      id: "TLS",
+      value: 82958,
+    },
+    {
+      id: "TTO",
+      value: 906943,
+    },
+    {
+      id: "TUN",
+      value: 149997,
+    },
+    {
+      id: "TUR",
+      value: 291449,
+    },
+    {
+      id: "TWN",
+      value: 445653,
+    },
+    {
+      id: "TZA",
+      value: 22623,
+    },
+    {
+      id: "UGA",
+      value: 575314,
+    },
+    {
+      id: "UKR",
+      value: 769461,
+    },
+    {
+      id: "URY",
+      value: 632622,
+    },
+    {
+      id: "USA",
+      value: 46640,
+    },
+    {
+      id: "UZB",
+      value: 324308,
+    },
+    {
+      id: "VEN",
+      value: 664229,
+    },
+    {
+      id: "VNM",
+      value: 197837,
+    },
+    {
+      id: "VUT",
+      value: 553997,
+    },
+    {
+      id: "PSE",
+      value: 859753,
+    },
+    {
+      id: "YEM",
+      value: 954213,
+    },
+    {
+      id: "ZAF",
+      value: 230896,
+    },
+    {
+      id: "ZMB",
+      value: 210913,
+    },
+    {
+      id: "ZWE",
+      value: 841169,
+    },
+    {
+      id: "KOR",
+      value: 543342,
+    },
+  ];
--- src/page/geography/Geography.jsx
+++ src/page/geography/Geography.jsx
@@ -1,11 +1,223 @@
-import React from 'react'
+import { Box, useTheme } from "@mui/material";
+import React from "react";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geo } from "./world_countries";
+import { data } from "./Data";
+
+
 
 const Geography = () => {
-  return (
-    <div>
-        Geography Page
-    </div>
-  )
-}
 
-export default Geography
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ height: "75vh" }}>
+      <ResponsiveChoropleth
+        data={data}
+        theme={{
+          text: {
+            fontSize: 11,
+            fill: "yello",
+            outlineWidth: 0,
+            outlineColor: "transparent",
+          },
+          axis: {
+            domain: {
+              line: {
+                stroke: "#777777",
+                strokeWidth: 1,
+              },
+            },
+            legend: {
+              text: {
+                fontSize: 12,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "transparent",
+              },
+            },
+            ticks: {
+              line: {
+                stroke: "#777777",
+                strokeWidth: 1,
+              },
+              text: {
+                fontSize: 11,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "transparent",
+              },
+            },
+          },
+          grid: {
+            line: {
+              stroke: "#dddddd",
+              strokeWidth: 0,
+            },
+          },
+          legends: {
+            title: {
+              text: {
+                fontSize: 11,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "transparent",
+              },
+            },
+            text: {
+              fontSize: 11,
+              fill: theme.palette.text.primary,
+              outlineWidth: 0,
+              outlineColor: "transparent",
+            },
+            ticks: {
+              line: {},
+              text: {
+                fontSize: 10,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "transparent",
+              },
+            },
+          },
+          annotations: {
+            text: {
+              fontSize: 13,
+              fill: theme.palette.text.primary,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+            link: {
+              stroke: "#000000",
+              strokeWidth: 1,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+            outline: {
+              stroke: "#000000",
+              strokeWidth: 2,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+            symbol: {
+              fill: "#000000",
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+          },
+          tooltip: {
+            wrapper: {},
+            container: {
+              background: theme.palette.background.default,
+              color: theme.palette.text.primary,
+              fontSize: 12,
+            },
+            basic: {},
+            chip: {},
+            table: {},
+            tableCell: {},
+            tableCellValue: {},
+          },
+        }}
+        features={geo.features}
+        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        colors="nivo"
+        domain={[0, 1000000]}
+        unknownColor="#666666"
+        label="properties.name"
+        valueFormat=".2s"
+        projectionTranslation={[0.5, 0.5]}
+        projectionRotation={[0, 0, 0]}
+        enableGraticule={true}
+        graticuleLineColor="#dddddd"
+        borderWidth={0.5}
+        borderColor="#152538"
+        defs={[
+          {
+            id: "dots",
+            type: "patternDots",
+            background: "inherit",
+            color: "#38bcb2",
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: "lines",
+            type: "patternLines",
+            background: "inherit",
+            color: "#eed312",
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+          {
+            id: "gradient",
+            type: "linearGradient",
+            colors: [
+              {
+                offset: 0,
+                color: "#000",
+              },
+              {
+                offset: 100,
+                color: "inherit",
+              },
+            ],
+          },
+        ]}
+        fill={[
+          {
+            match: {
+              id: "CAN",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "CHN",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "ATA",
+            },
+            id: "gradient",
+          },
+        ]}
+        legends={[
+          {
+            anchor: "bottom-left",
+            direction: "column",
+            justify: true,
+            translateX: 20,
+            translateY: -100,
+            itemsSpacing: 0,
+            itemWidth: 94,
+            itemHeight: 18,
+            itemDirection: "left-to-right",
+            itemTextColor: "#444444",
+            itemOpacity: 0.85,
+            symbolSize: 18,
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000000",
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </Box>
+  );
+};
+
+export default Geography;
