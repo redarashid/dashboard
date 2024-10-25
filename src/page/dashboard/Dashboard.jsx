d2a27281e24494352284dd8a2ff33d22e06c3acc
redarashid <redarashid824@gmail.com>
2024-10-26 01:26:27 +0300

    first commit

--- src/page/dashboard/Dashboard.jsx
+++ src/page/dashboard/Dashboard.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import Row1 from './Row1'
-import Row2 from './Row2'
-import Row3 from './Row3'
+import React from "react";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
+import { Box, Button } from "@mui/material";
+import { DownloadOutlined } from "@mui/icons-material";
 
 const Dashboard = () => {
   return (
     <div>
-        <Row1 />
-        <Row2 />
-        <Row3 />
+      <Box sx={{ textAlign: "right", mb: "1.3" }}>
+        <Button
+          sx={{ padding: "6px 8px", textTransform: "capitalize" }}
+          variant="contained"
+          color="primary">
+          <DownloadOutlined />
+          Download Reports
+        </Button>
+      </Box>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/page/dashboard/Row1.jsx
+++ src/page/dashboard/Row1.jsx
@@ -1,10 +1,21 @@
+import { Paper, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Row1 = () => {
   return (
-    <div>
-      
-    </div>
+    <Stack direction={"row"} sx={{display: "flex"}}>
+        <Paper sx={{minWidth: "333px"}}>
+            
+            <Stack >
+                icon
+                <Typography>12,361</Typography>
+                <Typography>Emails Sent</Typography>
+            </Stack>
+
+            <Stack></Stack>
+
+        </Paper>
+    </Stack>
   )
 }
 
