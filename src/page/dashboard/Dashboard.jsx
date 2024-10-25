d48c1bd98446bbb22fb99f22212fceec5286baf4
redarashid <redarashid824@gmail.com>
2024-10-26 01:10:40 +0300

    first commit

--- src/page/dashboard/Dashboard.jsx
+++ src/page/dashboard/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Row1 from './Row1'
+import Row2 from './Row2'
+import Row3 from './Row3'
 
 const Dashboard = () => {
   return (
     <div>
-        Dashboard Page
+        <Row1 />
+        <Row2 />
+        <Row3 />
     </div>
   )
 }
