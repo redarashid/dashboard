8310c9d5e46404e4c00efdc39f0f7326f71dcac6
redarashid <redarashid824@gmail.com>
2024-10-26 19:45:46 +0300

    first commit

--- src/page/dashboard/Row1.jsx
+++ src/page/dashboard/Row1.jsx
@@ -1,4 +1,4 @@
-import {Stack} from "@mui/material";
+import { Stack, useTheme } from "@mui/material";
 import React from "react";
 import Card from "./Card";
 import EmailIcon from "@mui/icons-material/Email";
@@ -7,14 +7,60 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
 
 const Row1 = () => {
+  const theme = useTheme();
   return (
-    <Stack direction={"row"} flexWrap={"wrap"} gap={1} justifyContent={{xs: "center", sm: "space-between"}}>
+    <Stack
+      direction={"row"}
+      flexWrap={"wrap"}
+      gap={1}
+      justifyContent={{ xs: "center", sm: "space-between" }}>
+      <Card
+        icon={
+          <EmailIcon
+            sx={{ fontSize: "23px", color: theme.palette.secondary.main }}
+          />
+        }
+        title={"12,361"}
+        subTitle={"Emails Sent"}
+        chart={""}
+        increase={"+14%"}
+      />
 
-      <Card icon={EmailIcon} title={"12,361"} subTitle={"Emails Sent"} chart={undefined} increase={"+14%"} />
-      <Card icon={PointOfSaleIcon} title={"431,225"} subTitle={"Sales obtained"} chart={undefined} increase={"+21%"} />
-      <Card icon={PersonAddIcon} title={"32,441"} subTitle={"New Clients"} chart={undefined} increase={"+5%"} />
-      <Card icon={TrafficIcon} title={"1,325,134"} subTitle={"TrafficIcon"} chart={undefined} increase={"+43%"} />
+      <Card
+        icon={
+          <PointOfSaleIcon
+            sx={{ fontSize: "23px", color: theme.palette.secondary.main }}
+          />
+        }
+        title={"431,225"}
+        subTitle={"Sales obtained"}
+        chart={""}
+        increase={"+21%"}
+      />
 
+      <Card
+        icon={
+          <PersonAddIcon
+            sx={{ fontSize: "23px", color: theme.palette.secondary.main }}
+          />
+        }
+        title={"32,441"}
+        subTitle={"New Clients"}
+        chart={""}
+        increase={"+5%"}
+      />
+
+      <Card
+        icon={
+          <TrafficIcon
+            sx={{ fontSize: "23px", color: theme.palette.secondary.main }}
+          />
+        }
+        title={"1,325,134"}
+        subTitle={"TrafficIcon"}
+        chart={""}
+        increase={"+43%"}
+      />
     </Stack>
   );
 };
