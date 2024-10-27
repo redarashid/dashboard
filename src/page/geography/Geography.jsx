e8e00aafecd161e5a0e767bbede08fbfbacb57fb
redarashid <redarashid824@gmail.com>
2024-10-27 15:31:43 +0300

    first commit

--- src/page/geography/Geography.jsx
+++ src/page/geography/Geography.jsx
@@ -3,172 +3,177 @@ import React from "react";
 import { ResponsiveChoropleth } from "@nivo/geo";
 import { geo } from "./world_countries";
 import { data } from "./Data";
+import Header from "components/Header";
 
 const Geography = ({ isDashbord = false }) => {
   const theme = useTheme();
 
   return (
-    <Box
-      sx={{
-        height: isDashbord ? "350px" : "75vh",
-        border: isDashbord ? null : `1px solid ${theme.palette.text.primary}`,
-        borderRadius: "3px",
-      }}>
-      <ResponsiveChoropleth
-        data={data}
-        theme={{
-          text: {
-            fontSize: 11,
-            fill: "yello",
-            outlineWidth: 0,
-            outlineColor: "transparent",
-          },
-          axis: {
-            domain: {
-              line: {
-                stroke: "#777777",
-                strokeWidth: 1,
-              },
-            },
-            legend: {
-              text: {
-                fontSize: 12,
-                fill: theme.palette.text.primary,
-                outlineWidth: 0,
-                outlineColor: "transparent",
-              },
-            },
-            ticks: {
-              line: {
-                stroke: "#777777",
-                strokeWidth: 1,
-              },
-              text: {
-                fontSize: 11,
-                fill: theme.palette.text.primary,
-                outlineWidth: 0,
-                outlineColor: "transparent",
-              },
-            },
-          },
-          grid: {
-            line: {
-              stroke: "#dddddd",
-              strokeWidth: 0,
-            },
-          },
-          legends: {
-            title: {
-              text: {
-                fontSize: 11,
-                fill: theme.palette.text.primary,
-                outlineWidth: 0,
-                outlineColor: "transparent",
-              },
-            },
+    <Box>
+      <Header title={"Geography"} subTitle={"Simple Geography Chart"} />
+
+      <Box
+        sx={{
+          height: isDashbord ? "350px" : "75vh",
+          border: isDashbord ? null : `1px solid ${theme.palette.text.primary}`,
+          borderRadius: "3px",
+        }}>
+        <ResponsiveChoropleth
+          data={data}
+          theme={{
             text: {
               fontSize: 11,
-              fill: theme.palette.text.primary,
+              fill: "yello",
               outlineWidth: 0,
               outlineColor: "transparent",
             },
-            ticks: {
-              line: {},
+            axis: {
+              domain: {
+                line: {
+                  stroke: "#777777",
+                  strokeWidth: 1,
+                },
+              },
+              legend: {
+                text: {
+                  fontSize: 12,
+                  fill: theme.palette.text.primary,
+                  outlineWidth: 0,
+                  outlineColor: "transparent",
+                },
+              },
+              ticks: {
+                line: {
+                  stroke: "#777777",
+                  strokeWidth: 1,
+                },
+                text: {
+                  fontSize: 11,
+                  fill: theme.palette.text.primary,
+                  outlineWidth: 0,
+                  outlineColor: "transparent",
+                },
+              },
+            },
+            grid: {
+              line: {
+                stroke: "#dddddd",
+                strokeWidth: 0,
+              },
+            },
+            legends: {
+              title: {
+                text: {
+                  fontSize: 11,
+                  fill: theme.palette.text.primary,
+                  outlineWidth: 0,
+                  outlineColor: "transparent",
+                },
+              },
               text: {
-                fontSize: 10,
+                fontSize: 11,
                 fill: theme.palette.text.primary,
                 outlineWidth: 0,
                 outlineColor: "transparent",
               },
-            },
-          },
-          annotations: {
-            text: {
-              fontSize: 13,
-              fill: theme.palette.text.primary,
-              outlineWidth: 2,
-              outlineColor: "#ffffff",
-              outlineOpacity: 1,
-            },
-            link: {
-              stroke: "#000000",
-              strokeWidth: 1,
-              outlineWidth: 2,
-              outlineColor: "#ffffff",
-              outlineOpacity: 1,
-            },
-            outline: {
-              stroke: "#000000",
-              strokeWidth: 2,
-              outlineWidth: 2,
-              outlineColor: "#ffffff",
-              outlineOpacity: 1,
-            },
-            symbol: {
-              fill: "#000000",
-              outlineWidth: 2,
-              outlineColor: "#ffffff",
-              outlineOpacity: 1,
-            },
-          },
-          tooltip: {
-            wrapper: {},
-            container: {
-              background: theme.palette.background.default,
-              color: theme.palette.text.primary,
-              fontSize: 12,
-            },
-            basic: {},
-            chip: {},
-            table: {},
-            tableCell: {},
-            tableCellValue: {},
-          },
-        }}
-        features={geo.features}
-        projectionScale={isDashbord ? 70 : 130}
-        margin={{ top: 20, right: 0, bottom: 0, left: 0 }}
-        colors="RdBu"
-        domain={[0, 1000000]}
-        unknownColor="#666666"
-        label="properties.name"
-        valueFormat=".2s"
-        projectionTranslation={[0.5, 0.5]}
-        projectionRotation={[0, 0, 0]}
-        enableGraticule={false}
-        graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        legends={
-          isDashbord
-            ? []
-            : [
-                {
-                  anchor: "bottom-left",
-                  direction: "column",
-                  justify: true,
-                  translateX: 20,
-                  translateY: -40,
-                  itemsSpacing: 0,
-                  itemWidth: 94,
-                  itemHeight: 18,
-                  itemDirection: "left-to-right",
-                  itemTextColor: theme.palette.text.primary,
-                  itemOpacity: 0.85,
-                  symbolSize: 18,
-                  effects: [
-                    {
-                      on: "hover",
-                      style: {
-                        itemTextColor: "#000000",
-                        itemOpacity: 1,
-                      },
-                    },
-                  ],
+              ticks: {
+                line: {},
+                text: {
+                  fontSize: 10,
+                  fill: theme.palette.text.primary,
+                  outlineWidth: 0,
+                  outlineColor: "transparent",
                 },
-              ]
-        }
-      />
+              },
+            },
+            annotations: {
+              text: {
+                fontSize: 13,
+                fill: theme.palette.text.primary,
+                outlineWidth: 2,
+                outlineColor: "#ffffff",
+                outlineOpacity: 1,
+              },
+              link: {
+                stroke: "#000000",
+                strokeWidth: 1,
+                outlineWidth: 2,
+                outlineColor: "#ffffff",
+                outlineOpacity: 1,
+              },
+              outline: {
+                stroke: "#000000",
+                strokeWidth: 2,
+                outlineWidth: 2,
+                outlineColor: "#ffffff",
+                outlineOpacity: 1,
+              },
+              symbol: {
+                fill: "#000000",
+                outlineWidth: 2,
+                outlineColor: "#ffffff",
+                outlineOpacity: 1,
+              },
+            },
+            tooltip: {
+              wrapper: {},
+              container: {
+                background: theme.palette.background.default,
+                color: theme.palette.text.primary,
+                fontSize: 12,
+              },
+              basic: {},
+              chip: {},
+              table: {},
+              tableCell: {},
+              tableCellValue: {},
+            },
+          }}
+          features={geo.features}
+          projectionScale={isDashbord ? 70 : 130}
+          margin={{ top: 20, right: 0, bottom: 0, left: 0 }}
+          colors="RdBu"
+          domain={[0, 1000000]}
+          unknownColor="#666666"
+          label="properties.name"
+          valueFormat=".2s"
+          projectionTranslation={[0.5, 0.5]}
+          projectionRotation={[0, 0, 0]}
+          enableGraticule={false}
+          graticuleLineColor="#dddddd"
+          borderWidth={0.5}
+          borderColor="#152538"
+          legends={
+            isDashbord
+              ? []
+              : [
+                  {
+                    anchor: "bottom-left",
+                    direction: "column",
+                    justify: true,
+                    translateX: 20,
+                    translateY: -40,
+                    itemsSpacing: 0,
+                    itemWidth: 94,
+                    itemHeight: 18,
+                    itemDirection: "left-to-right",
+                    itemTextColor: theme.palette.text.primary,
+                    itemOpacity: 0.85,
+                    symbolSize: 18,
+                    effects: [
+                      {
+                        on: "hover",
+                        style: {
+                          itemTextColor: "#000000",
+                          itemOpacity: 1,
+                        },
+                      },
+                    ],
+                  },
+                ]
+          }
+        />
+      </Box>
     </Box>
   );
 };
--- src/page/line/Line.jsx
+++ src/page/line/Line.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ChartLine from "./ChartLine";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Line = () => {
-  return <ChartLine />;
+  return (
+    <Box>
+      <Header title={"Line Chart"} subTitle={"Simple Line Chart"} />
+      <ChartLine />
+    </Box>
+  );
 };
 
 export default Line;
