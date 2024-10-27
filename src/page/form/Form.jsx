03846eb154ec4f235e1699ec2b4b7588f27e0fa3
redarashid <redarashid824@gmail.com>
2024-10-27 15:19:14 +0300

    first commit

--- src/page/form/Form.jsx
+++ src/page/form/Form.jsx
@@ -4,6 +4,7 @@ import TextField from "@mui/material/TextField";
 import { Alert, Button, Snackbar, Stack } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import { useForm } from "react-hook-form";
+import Header from "components/Header";
 
 const data = [
   {
@@ -54,92 +55,100 @@ const Form = () => {
   };
 
   return (
-    <Box
-      onSubmit={handleSubmit(onSubmit)}
-      component="form"
-      // sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 3,
-      }}
-      noValidate
-      autoComplete="off">
-      <Stack direction={"row"}>
+    <Box>
+      <Header title={"CREATE USER"} subTitle={"Create a New User Profile"} />
+      <Box
+        onSubmit={handleSubmit(onSubmit)}
+        component="form"
+        // sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+        noValidate
+        autoComplete="off">
+        <Stack direction={"row"}>
+          <TextField
+            error={Boolean(errors.firstName)}
+            helperText={
+              Boolean(errors.firstName)
+                ? "This field is required & min 3 character"
+                : null
+            }
+            {...register("firstName", { required: true, minLength: 3 })}
+            label="First Name"
+            variant="filled"
+            sx={{ flex: 1, mr: "15px" }}
+          />
+          <TextField
+            error={Boolean(errors.lastName)}
+            helperText={
+              Boolean(errors.lastName)
+                ? "This field is required & min 3 character"
+                : null
+            }
+            {...register("lastName", { required: true, minLength: 3 })}
+            label="Last Name"
+            variant="filled"
+            sx={{ flex: 1 }}
+          />
+        </Stack>
+
         <TextField
-          error={Boolean(errors.firstName)}
+          error={Boolean(errors.email)}
           helperText={
-            Boolean(errors.firstName)
-              ? "This field is required & min 3 character"
+            Boolean(errors.email)
+              ? "Please provide a valid email address"
               : null
           }
-          {...register("firstName", { required: true, minLength: 3 })}
-          label="First Name"
+          {...register("email", { required: true, pattern: regEmail })}
+          label="Email"
           variant="filled"
-          sx={{ flex: 1, mr: "15px" }}
         />
         <TextField
-          error={Boolean(errors.lastName)}
+          error={Boolean(errors.contactNumber)}
           helperText={
-            Boolean(errors.lastName)
-              ? "This field is required & min 3 character"
+            Boolean(errors.contactNumber)
+              ? "Please provide a valid Phone number"
               : null
           }
-          {...register("lastName", { required: true, minLength: 3 })}
-          label="Last Name"
+          {...register("contactNumber", {
+            required: true,
+            pattern: phoneRegExp,
+          })}
+          label="Contact Number"
           variant="filled"
-          sx={{ flex: 1 }}
         />
-      </Stack>
+        <TextField label="Adress 1" variant="filled" />
+        <TextField label="Adress 2" variant="filled" />
 
-      <TextField
-        error={Boolean(errors.email)}
-        helperText={
-          Boolean(errors.email) ? "Please provide a valid email address" : null
-        }
-        {...register("email", { required: true, pattern: regEmail })}
-        label="Email"
-        variant="filled"
-      />
-      <TextField
-        error={Boolean(errors.contactNumber)}
-        helperText={
-          Boolean(errors.contactNumber)
-            ? "Please provide a valid Phone number"
-            : null
-        }
-        {...register("contactNumber", { required: true, pattern: phoneRegExp })}
-        label="Contact Number"
-        variant="filled"
-      />
-      <TextField label="Adress 1" variant="filled" />
-      <TextField label="Adress 2" variant="filled" />
+        <TextField variant="filled" select label="Role" defaultValue="User">
+          {data.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
 
-      <TextField variant="filled" select label="Role" defaultValue="User">
-        {data.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </TextField>
+        <Box sx={{ textAlign: "right" }}>
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{ textTransform: "capitalize" }}>
+            create new user
+          </Button>
 
-      <Box sx={{ textAlign: "right" }}>
-        <Button
-          variant="contained"
-          type="submit"
-          sx={{ textTransform: "capitalize" }}>
-          create new user
-        </Button>
-
-        <Snackbar
-          anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          open={open}
-          autoHideDuration={3000}
-          onClose={handleClose}>
-          <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
-            Account created successfully
-          </Alert>
-        </Snackbar>
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}>
+            <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
+              Account created successfully
+            </Alert>
+          </Snackbar>
+        </Box>
       </Box>
     </Box>
   );
